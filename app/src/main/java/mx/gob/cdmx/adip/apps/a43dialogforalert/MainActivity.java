c4db0729e626314b55453f269e2d1ff72b651706
CrisM93 <criztian.gio@gmail.com>
2023-01-10 10:45:23 -0600

    Nuevo mensaje

---
+++ app/src/main/java/mx/gob/cdmx/adip/apps/a43dialogforalert/MainActivity.java
@@ -0,0 +1,44 @@
+package mx.gob.cdmx.adip.apps.a43dialogforalert;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+     public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder =  new AlertDialog.Builder(MainActivity.this);
+
+        // Set the dialog title and message.
+        myAlertBuilder.setTitle(R.string.alert_title);
+        myAlertBuilder.setMessage(R.string.alert_message);
+
+        // Add the dialog buttons.
+        myAlertBuilder.setPositiveButton(R.string.ok_button,  new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), R.string.pressed_ok, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        myAlertBuilder.setNegativeButton(R.string.cancel_button,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User cancelled the dialog.
+                        Toast.makeText(getApplicationContext(), R.string.pressed_cancel, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        // Create and show the AlertDialog.
+        myAlertBuilder.show();
+    }
+}
